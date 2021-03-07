f1ab85c42f0176416ab1977f1d1353babffa3774
jefferson <jefferson@MacBook-Pro-de-jefferson.local>
2021-03-07 11:52:47 -0800

    menu mobile

---
+++ src/components/HeaderLandingPage/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import logo from '../../assets/logo_landing.png';
+import styles from './styles.module.css';
+
+function HeaderLandingPage() {
+  const history = useHistory();
+
+  return (
+    <header className={styles.container}>
+      <input type="checkbox" id="check" />
+      <a href="/">
+        <img src={logo} alt=" " />
+      </a>
+      <label htmlFor="check">
+        <span className="iconify" data-icon="ph:list" data-inline="false" />
+      </label>
+      <ul className="menu_mobile">
+        <li>
+          <NavLink to="/">Início</NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup">Cadastre-se</NavLink>
+        </li>
+        <li>
+          <button type="button" onClick={() => history.push('/signin')}>
+            Entrar
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default HeaderLandingPage;
--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import HeaderLandingPage from '../../components/HeaderLandingPage';
+import ilustration from '../../assets/ilustration.svg';
 import styles from './styles.module.css';
 
 function LandingPage() {
-  return <div className={styles.container}>img</div>;
+  return (
+    <div className={styles.container}>
+      <HeaderLandingPage />
+      <main>
+        <h1>Conheça a sua nova comunidade colaborativa</h1>
+        <p>
+          A plataforma Joindev foi criada para facilitar a comunicação entre
+          profissionais e empresas.
+        </p>
+        <img src={ilustration} alt=" " />
+      </main>
+    </div>
+  );
 }
 
 export default LandingPage;
