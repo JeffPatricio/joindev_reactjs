2580bf953c856d28a504644113fe15d0ca15cc24
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-04-05 22:41:21 -0300

    list vacancies

--- src/components/CardVacancies/index.js
+++ src/components/CardVacancies/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
-import logojoindev from '../../../assets/logojoindev.png';
+import logojoindev from '../../assets/logojoindev.png';
 
 function CardVacancies() {
   return (
--- src/pages/Vacancies/index.js
+++ src/pages/Vacancies/index.js
@@ -1,15 +1,16 @@
 import React, { useRef } from 'react';
-import { Form } from '@unform/web';
 import HeaderPanel from '../../components/HeaderPanel';
-import Input from '../../components/Input';
 import Button from '../../components/Button';
 import styles from './styles.module.css';
+import CardVacancies from '../../components/CardVacancies';
 
 function Vacancies() {
   const formRef = useRef(null);
   const buttonRef = useRef(null);
+  const [search, setSearch] = React.useState('');
 
-  async function handleSubmit() {
+  async function handleSubmit(e) {
+    e.preventDefault();
     console.log('handle');
   }
 
@@ -24,13 +25,59 @@ function Vacancies() {
             data-icon="ph:plus-light"
             data-inline="false"
           />
-          Criar postagem
+          Criar vaga
         </button>
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <Input type="email" placeholder="E-mail" name="email" />
-          <Input type="password" placeholder="Senha" name="password" />
-          <Button ref={buttonRef} type="submit" text="Entrar" />
-        </Form>
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <div className={styles.search}>
+            {!search && (
+              <div>
+                <span
+                  className="iconify"
+                  data-icon="uil:search"
+                  data-inline="false"
+                />
+              </div>
+            )}
+            {!!search && (
+              <div
+                className={styles.close}
+                onClick={() => {
+                  setSearch('');
+                }}
+              >
+                <span
+                  className="iconify"
+                  data-icon="ph:x-light"
+                  data-inline="false"
+                />
+              </div>
+            )}
+            <input
+              type="text"
+              placeholder="Pesquisar por título ou descrição"
+              value={search}
+              onChange={(e) => setSearch(e.currentTarget.value)}
+            />
+          </div>
+          <Button ref={buttonRef} type="submit" text="Buscar" />
+        </form>
+        <div className={styles.containerlist}>
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+          <CardVacancies />
+        </div>
       </div>
     </div>
   );
