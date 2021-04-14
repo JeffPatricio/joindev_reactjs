f42ac4ae95cf8b20409b7e2f1e74aabc60ed1e04
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-04-13 23:15:40 -0300

    atualizacao meu perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,27 +1,58 @@
 import React from 'react';
+import { Form } from '@unform/web';
 import HeaderPanel from '../../components/HeaderPanel';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Form } from '@unform/web';
+import { useAuth } from '../../contexts/AuthContext';
 import styles from './styles.module.css';
 
 function Profile() {
+  const inputFileRef = React.useRef(null);
+  const { authUser } = useAuth();
+
+  const [linkImage, setLinkImage] = React.useState(authUser.photo);
+
   return (
     <div className={styles.container}>
       <HeaderPanel />
-      <Form>
+      <Form
+        initialData={{
+          name: authUser.name,
+          email: authUser.email,
+        }}
+      >
         <p>Meu Perfil</p>
         <img
           alt=""
           id="img_profile"
-          src="https://i.ibb.co/dDpxVpV/Group-50.png"
+          src={linkImage || 'https://i.ibb.co/dDpxVpV/Group-50.png'}
         />
         <div className={styles.containerButtonsImage}>
-          <button>Remover foto</button>
-          <button>Alterar foto</button>
+          <button
+            type="button"
+            onClick={() => {
+              setLinkImage(null);
+            }}
+          >
+            Remover foto
+          </button>
+          <button onClick={() => inputFileRef.current.click()} type="button">
+            Alterar foto
+          </button>
         </div>
+        <input
+          type="file"
+          ref={inputFileRef}
+          accept="image/png, image/jpeg"
+          onChange={() => {
+            const linkImage = URL.createObjectURL(
+              inputFileRef.current.files[0]
+            );
+            setLinkImage(linkImage);
+          }}
+        />
         <Input type="text" label="Nome" name="name" />
-        <Input type="text" label="E-mail" name="email" disabled />
+        <Input type="email" label="E-mail" name="email" disabled />
         <div className={styles.checkbox}>
           <Input name="isCompany" type="checkbox" id="isCompany" />
           <label htmlFor="isCompany">Sou uma empresa</label>
